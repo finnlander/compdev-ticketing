a483000ea8f5fa40af53d0515722696dba47b273
Janne Suomalainen <janne.suomalainen@vincit.fi>
2022-01-15 19:26:25 +0200

    Add descriptions into some files
    Added some description into files so it is easier
    to get an impression what the file contains.

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,19 +1,22 @@
-import mongoose from 'mongoose';
-import { Password } from '../services/password';
+/**
+ * A mongoose models for the user object.
+ */
+import mongoose from 'mongoose'
+import { Password } from '../services/password'
 
 interface UserAttrs {
-    email: string;
+    email: string
     password: string
 }
 
 interface UserModel extends mongoose.Model<UserDoc> {
 
-    build(attrs: UserAttrs): UserDoc;
+    build(attrs: UserAttrs): UserDoc
 }
 
 export interface UserDoc extends mongoose.Document {
-    email: string;
-    password: string;
+    email: string
+    password: string
 }
 
 const userSchema = new mongoose.Schema({
@@ -28,27 +31,27 @@ const userSchema = new mongoose.Schema({
 }, {
     toJSON: {
         transform(doc, ret) {
-            delete ret.password;
-            ret.id = ret._id;
-            delete ret._id;
+            delete ret.password
+            ret.id = ret._id
+            delete ret._id
         },
         versionKey: false
     }
-});
+})
 
 userSchema.statics.build = (attrs: UserAttrs) => {
-    return new User(attrs);
-};
+    return new User(attrs)
+}
 
 userSchema.pre('save', async function (done) {
     if (this.isModified('password')) {
-        const hashedPassword = await Password.toHash(this.get('password'));
-        this.set('password', hashedPassword);
+        const hashedPassword = await Password.toHash(this.get('password'))
+        this.set('password', hashedPassword)
     }
 
-    done();
-});
+    done()
+})
 
-const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema)
 
-export { User };
+export { User }
